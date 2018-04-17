2b723e3265d717d67bfafa3ebe7016d5977cee20
prbhasva <prbhavsa@my.bridgeport.edu>
2018-04-16 23:11:29 -0400

    Delaying Time and Weather details

--- maincontents.js
+++ maincontents.js
@@ -157,7 +157,8 @@ function ShowPopup(){
 }
 
 $(document).ready(function(){
-    ShowTime();
+
+
 
     $('a.btn-ok, #dialog-overlay, #dialog-box').click(function () {
         $('#dialog-overlay, #dialog-box').hide();
@@ -166,30 +167,42 @@ $(document).ready(function(){
 })
 
 
-var city = "San Antonio";
-var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='f'";
-var queryURL = "https://query.yahooapis.com/v1/public/yql?q="+ searchtext + "&format=json";
+setTimeout(function () {
+    ShowTime();
+    console.log("Time Showed");
 
-$.getJSON(queryURL, function (data) {
+    var city = "San Antonio";
+    var searchtext = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "') and u='f'";
+    var queryURL = "https://query.yahooapis.com/v1/public/yql?q="+ searchtext + "&format=json";
 
-    var results = data.query.results;
-    var firstResult = results.channel.item.condition;
-    console.log(firstResult);
+    $.getJSON(queryURL, function (data) {
+
+        var results = data.query.results;
+        var firstResult = results.channel.item.condition;
+        console.log(firstResult);
 
 
-    var location = 'Unknown'; // not returned in response
-    var temp = firstResult.temp;
-    var text = firstResult.text;
-    var image =  firstResult.code;
-    var loc = 'https://s.yimg.com/zz/combo?a/i/us/we/52/'+image+'.gif' ;
+        var location = 'Unknown'; // not returned in response
+        var temp = firstResult.temp;
+        var text = firstResult.text;
+        var image =  firstResult.code;
+        var loc = 'https://s.yimg.com/zz/combo?a/i/us/we/52/'+image+'.gif' ;
 
-    // $('#temp').append('The temperature is <strong>' + temp + '</strong><sup>°F</sup> Forecast calls for '+text);
+        // $('#temp').append('The temperature is <strong>' + temp + '</strong><sup>°F</sup> Forecast calls for '+text);
 
-    $('#condition').append(text);
-    $('#temp').append(temp+ '</strong><sup>°F</sup>');
+        $('#condition').append(text);
+        $('#temp').append(temp+ '</strong><sup>°F</sup>');
 
-    $('#image-zoom').attr("src",loc);
-});
+        $('#image-zoom').attr("src",loc);
+
+        console.log("Weather Showed");
+    });
+
+
+
+}, 2000);
+
+
 
 function ShowTime()
 {
