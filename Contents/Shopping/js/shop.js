f5392502e4d8aa071dba773d681b2924817d2889
tamoorkayani <tamoorkayani@gmail.com>
2018-05-09 17:08:20 +0500

    Screen saver update
    Screen saver update

--- Contents/Shopping/js/shop.js
+++ Contents/Shopping/js/shop.js
@@ -3,7 +3,7 @@
 //-----------------------------------------------------
 
 function redirect(){
-    window.location.href = "../../maincontents.htm";
+    window.location.href = "../../main.htm";
 }
 var initial=setTimeout(redirect,60000);
 
